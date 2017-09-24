348f720f68adc177e21c38c9559c654ccfb81603
aldoso <alexdobras@gmail.com>
2017-09-25 02:36:20 +0300

    new corrections

---
+++ index.js
@@ -0,0 +1,19 @@
+$('body').scrollspy({
+    target: '#list-example'
+})
+
+//remove class for mobile // $(window).resize(function() { // If($(window).width() < 576) { // $( '.sticky-top').removeClass( 'sticky-top'); // } // });
+
+
+// Changing the defaults
+window.sr = ScrollReveal({
+    reset: true
+});
+
+// Customizing a reveal set
+sr.reveal('.maincontent', {
+    duration: 777,
+    origin: 'down',
+    distance: '2px',
+    mobile: false //no animation for mobile devices
+});
